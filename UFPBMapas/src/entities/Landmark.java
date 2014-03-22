9b6974d7e0891a926046e72622831f2f2bff6364
Lauciano <jose.lauciano@gmail.com>
2014-03-22 15:49:47 -0300

    Estrutura de dados
    Estrutura de dados do Landmark estruturada

--- UFPBMapas/src/entities/Landmark.java
+++ UFPBMapas/src/entities/Landmark.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class Landmark implements Serializable {
@@ -8,12 +9,14 @@ public class Landmark implements Serializable {
 	private String name;
 	private int picture;
 	private String description;
+	private ArrayList<Integer> directDestinations;
 	
 	public Landmark(int id, String name, int picture, String description){
 		this.id = id;
 		this.name = name;
 		this.picture = picture;
 		this.description = description;
+		this.directDestinations = null;
 	}
 	
 	public int getId() {
@@ -27,20 +30,41 @@ public class Landmark implements Serializable {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public int getPicture() {
 		return picture;
 	}
+	
 	public void setPicture(int picture) {
 		this.picture = picture;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	public ArrayList<Integer> getDirectDestinations() {
+		return directDestinations;
+	}
+	
+	public void setDirectDestinations(ArrayList<Integer> directDestinations) {
+		this.directDestinations = directDestinations;
+	}
+	
+	public int getDestination(int id) {
+		return directDestinations.get(id);
+	}
+	
+	public void setDestination(int id, int destination) {
+		this.directDestinations.set(id, destination);
 	}
 	
 	public void copyLandmark(int id, String name, int picture, String description){
