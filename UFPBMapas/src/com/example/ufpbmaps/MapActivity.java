1d1077a77ea2dbdb0c0144ee7fb66e5d8f644506
Ana Beatrice <anabeatricesevero@gmail.com>
2014-03-22 20:30:06 -0300

    Mapa da UFPB com zoom :)

--- UFPBMapas/src/com/example/ufpbmaps/MapActivity.java
+++ UFPBMapas/src/com/example/ufpbmaps/MapActivity.java
@@ -11,6 +11,7 @@ public class MapActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);	
+
 	}
 	
 	public boolean onCreateOptionsMenu(Menu menu) {
