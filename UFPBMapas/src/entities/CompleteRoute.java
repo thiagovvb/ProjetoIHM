bc3213906c86f3acda4c0691ad3bb6f51689fc14
Lauciano <jose.lauciano@gmail.com>
2014-03-23 08:26:02 -0300

    Grafo - Parte 3 eu acho...
    Grafo quase pronto, só falta pegar as rotas no banco de dados e tentar
    fazer algo para diminuir a quantidade de memória consumida, pq ela está
    absurda.

--- UFPBMapas/src/entities/CompleteRoute.java
+++ UFPBMapas/src/entities/CompleteRoute.java
@@ -1,5 +1,42 @@
 package entities;
 
-public class CompleteRoute {
+import java.util.ArrayList;
 
+import android.content.Context;
+
+public class CompleteRoute {
+	private ArrayList<Route> completeRoute;
+
+	public CompleteRoute(Context context) {
+		completeRoute = new ArrayList<Route>();
+	}
+	
+	public ArrayList<Route> getCompleteRoute() {
+		return completeRoute;
+	}
+
+	public void setCompleteRoute(ArrayList<Route> completeRoute) {
+		this.completeRoute = completeRoute;
+	}
+	
+	public Route getRoute(int id) {
+		return completeRoute.get(id);
+	}
+	
+	public void setRoute(int id, Route route) {
+		this.completeRoute.set(id, route);
+	}
+	
+	public void create(int source, int destination) {
+		Graph.getInstance().dijkstra(source, destination);
+		int j = Graph.getInstance().getDist().length;
+		int aux = destination;
+		//eu acho que este laço vai dar infinito
+		while(aux != source) {
+			for(int i = 0; i < j; i++) {
+				completeRoute.add(Graph.getInstance().getRoute(Graph.getInstance().getPrev()[aux], aux));
+				aux = Graph.getInstance().getPrev()[aux];
+			}
+		}
+	}
 }
--- UFPBMapas/src/entities/Graph.java
+++ UFPBMapas/src/entities/Graph.java
@@ -2,14 +2,61 @@ package entities;
 
 import java.util.ArrayList;
 
+import dbclasses.DataHandler;
+
+import android.content.Context;
+
 public class Graph {
-	private ArrayList<Route>	 route;
+	private Context context;
+	private ArrayList<Landmark> landmark;
+	private ArrayList<Route> route;
+	private int[] dist;
+	private int[] prev;
 	private static Graph uniqueInstance = null;
+	
+	private Graph() {
+		DataHandler dh = new DataHandler(context);
+		landmark = dh.fetchLandmark();
+		route = new ArrayList<Route>(); //só enquanto não coisa o bd
+		//route = dh.fetchRoute();
+	}
 	
 	public static Graph getInstance() {
 		if(uniqueInstance == null)
 			return new Graph();
 		return uniqueInstance;
+	}
+
+	public Context getContext() {
+		return context;
+	}
+
+	public void setContext(Context context) {
+		this.context = context;
+	}
+
+	public void setDist(int[] dist) {
+		this.dist = dist;
+	}
+
+	public void setPrev(int[] prev) {
+		this.prev = prev;
+	}
+
+	public ArrayList<Landmark> getAllLandmarks() {
+		return landmark;
+	}
+	
+	public void setAllLandmarks(ArrayList<Landmark> landmark) {
+		this.landmark = landmark;
+	}
+	
+	public Landmark getLandmark(int id) {
+		return landmark.get(id);
+	}
+	
+	public void setLandmark(int id, Landmark landmark) {
+		this.landmark.set(id, landmark);
 	}
 	
 	public ArrayList<Route> getAllRoutes() {
@@ -24,11 +71,63 @@ public class Graph {
 		return route.get(id);
 	}
 	
+	public Route getRoute(int source, int destination) {
+		for(Route r : route) {
+			if(r.getSourceId() == source && r.getDestinationId() == destination) {
+				return r;
+			}
+		}
+		return null;
+	}
+	
 	public void setRoute(int id, Route route) {
 		this.route.set(id, route);
 	}
 	
+	public int[] getDist() {
+		return dist;
+	}
+	
+	public int[] getPrev() {
+		return prev;
+	}
+	
 	public String toString() {
 		return "Grafo com " + route.size() + " rotas.";
+	}
+	
+	public void dijkstra(int source, int destination) {
+		dist = new int[landmark.size()];
+		prev = new int[landmark.size()];
+		ArrayList<Integer> q = new ArrayList<Integer>();
+		for (Landmark l : landmark) {
+			dist[l.getId()] = Integer.MAX_VALUE;
+			prev[l.getId()] = -1;
+			q.set(l.getId(), l.getId());
+		}
+		dist[source] = 0;
+		while(q.size() == 0) {
+			int i = -1;
+			int u = Integer.MAX_VALUE;
+			for(Route r : route){
+				u = Math.min(u, r.getDist());
+				if(r.getDist() <= u)
+					i = r.getSourceId();
+			}
+			q.remove(i);
+			if(dist[i] == Integer.MAX_VALUE)
+				break;
+			for(Route r : route){
+				if(r.getSourceId() == i){
+					int v = r.getDestinationId();
+					int alt = dist[i] + r.getDist();
+					if(alt < dist[v]) {
+						dist[v] = alt;
+						prev[v] = u;
+						q.remove(v);
+					}
+				}
+			}
+		}
 	}
 }
