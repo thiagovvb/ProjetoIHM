8260561f8920fd4abbf0d0b3061830d420778ce8
Elaine Anita de Melo Gomes Soares <elaineanita1@gmail.com>
2014-03-22 22:57:52 -0300

    QRCode do DestinationLandmark

--- UFPBMapas/src/com/example/ufpbmaps/DestinationLandmarkActivity.java
+++ UFPBMapas/src/com/example/ufpbmaps/DestinationLandmarkActivity.java
@@ -1,8 +1,13 @@
 package com.example.ufpbmaps;
 
+import java.util.StringTokenizer;
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -40,7 +45,7 @@ public class DestinationLandmarkActivity extends Activity {
 		Button destination = new Button(this);
 		destination.setId(0);
 		destination.setText(R.string.qrcode_button);
-		destination.setOnClickListener(null);
+		destination.setOnClickListener(lerQR);
 		l1.addView(destination);
 		for(int j = 1; j <= 23; j++){
 			if(j != landmark.getId()){
@@ -86,4 +91,77 @@ public class DestinationLandmarkActivity extends Activity {
      
         return super.onOptionsItemSelected(item);
     }
+	
+	/*
+	 * 
+	 * INÍCIO: TRATAMENTO BOTÃO QRCODE
+	 * 
+	 */
+	
+	protected static final String ZXING_MARKET = "market://search?q=pname:com.google.zxing.client.android";
+	protected static final String ZXING_DIRECT = "https://zxing.googlecode.com/files/BarcodeScanner3.1.apk";
+	public String QRCode = " ";
+
+	/**
+	 * Método que será chamado no clique do botão
+	 * 
+	 * @param view
+	 */
+	public OnClickListener lerQR = new OnClickListener() {		
+		public void onClick(View v) {
+			// Criamos um Intent com o caminho aplicativo que irá ler o QR code
+			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+			intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			// Passamos o parametro "SCAN_MODE" para ler somente códigos no formato QR
+			try {
+				startActivityForResult(intent, 0);
+			} catch (ActivityNotFoundException e) {
+				mostrarMensagem();
+			}
+		}
+	};
+
+	/**
+	 * Pergnta se o usuário deseja instalar o ZXing
+	 */
+	private void mostrarMensagem() {
+		new AlertDialog.Builder(this)
+				.setTitle("Instalar barcode scanner?")
+				.setMessage(
+						"Para escanear QR code você precisa instalar o ZXing barcode scanner.")
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setPositiveButton("Instalar",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int whichButton) {
+								Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ZXING_MARKET));
+								try {
+									startActivity(intent);
+								} catch (ActivityNotFoundException e) {
+									//Se não tiver o Play Store
+									intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ZXING_DIRECT));
+									startActivity(intent);
+								}
+							}
+						}).setNegativeButton("Cancelar", null).show();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+			QRCode = intent.getStringExtra("SCAN_RESULT");
+
+			int key = Integer.parseInt(accessCode());
+			Landmark landmark = DBLandmark.getInstance().getLdm(key);
+			Intent intent2 = new Intent(this, LandmarkActivity.class);
+			intent2.putExtra("Landmark", landmark);
+			startActivity(intent2);			
+		}
+	}
+	
+	public String accessCode(){		 
+		StringTokenizer tokens = new StringTokenizer(QRCode);
+		tokens.nextToken();
+		return (String) tokens.nextToken();
+	}
+	
 }
